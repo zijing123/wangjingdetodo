440b4be041e0cd8db9761b0c84cca6d591b350ea
ziing <2651217350@qq.com>
2016-03-23 19:42:37 +0800

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wangjingdetodoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wangjingdetodoApp
+ */
+angular.module('wangjingdetodoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wangjingdetodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wangjingdetodoApp
+ */
+angular.module('wangjingdetodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
